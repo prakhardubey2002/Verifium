41e00f34b3554a17e57d78301b5ef3e122ba8df6
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-01-02 09:50:44 +0530

    Footer added button fixed,overflow removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import '../css/Footer.css';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="upper">
+        <div className="first">
+          <h2>Verifium</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, aperiam! Lorem ipsum dolor sit amet, <br/>consectetur adipisicing elit. Ullam, vero.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Sit neque aut iusto laborum tempore nobis saepe, nam ducimus corporis rem!
+          </p>
+        </div>
+        <div className="links">
+          <h2>Useful link</h2>
+          <ul>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Linkdln</li>
+            <li>Github</li>
+          </ul>
+        </div>
+        <div className="newseletter">
+          <h2>Subscribe</h2>
+          <input type="text" placeholder='Email' />
+        </div>
+      </div><br /><br />
+      <div className="lower">
+        <p>
+
+      Copyright © 2023 verifium All rights reserved.
+        </p>
+      </div>
+    </div>
   )
 }
 
