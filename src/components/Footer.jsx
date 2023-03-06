29f6063c783bc08a8587bcafcdb1b5331b19e80f
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-03-06 23:42:04 +0530

    FFooter's feedback integration added,Footer css updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/Footer.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { toast } from 'react-toastify';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 const Footer = () => {
+  const [feedback, setFeedback] = useState('');
+  const Submit = async (e) => {
+    if (e) {
+      e.preventDefault();
+    }
+    const formData = {
+      feedback,
+    };
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    };
+
+    const response = await fetch('http://localhost:3000/ali/createfeedback', options);
+    const data = await response.json();
+    toast(`Feedback added Succesfully `);
+    console.log(data);
+    setFeedback("")
+  };
   return (
     <div className="footer">
       <div className="upper">
         <div className="first">
           <h2>Verifium</h2>
-          <p>Our project offers educational organizations to have their own Digital credentials offering page to give tamper proof badges, <br/>certificates, a degree in NFT format with a verification system to verify credentials
+          <p>Our project offers educational organizations to have their own Digital credentials offering page to give tamper proof badges, <br />certificates, a degree in NFT format with a verification system to verify credentials
           </p>
         </div>
         <div className="links">
@@ -16,19 +39,24 @@ const Footer = () => {
           <ul>
             {/* <li>Facebook</li>
             <li>Instagram</li> */}
-            <li>Linkdln <LinkedInIcon/></li>
-            <li>Github <GitHubIcon/></li>
+            <li>Linkdln <LinkedInIcon /></li>
+            <li>Github <GitHubIcon /></li>
           </ul>
         </div>
         <div className="newseletter">
-          <h2>Subscribe</h2>
-          <input type="text" placeholder='Email' />
+          <h2>Feedback</h2>
+          <div className="finp">
+
+          <input type="text" placeholder='Feedback' value={feedback} onChange={(e)=>{setFeedback(e.target.value)}} />
+          <ArrowForwardIcon className='icon'  onClick={(e)=>Submit(e)}  />
+          
+          </div>
         </div>
       </div><br /><br />
       <div className="lower">
         <p>
 
-      Copyright © 2023 verifium All rights reserved.
+          Copyright © 2023 verifium All rights reserved.
         </p>
       </div>
     </div>
